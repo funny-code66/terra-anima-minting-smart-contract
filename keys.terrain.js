da66020b7d5508f404335ba72ce65963e359ebfd
Haulerkonj <86504841+haulerkonj@users.noreply.github.com>
2022-04-11 20:34:04 +0000

    Revert keys.terrain.js

--- keys.terrain.js
+++ keys.terrain.js
@@ -7,6 +7,6 @@ module.exports = {
       "awkward escape town eight expect time custom park sweet anchor advance defense remind tooth shine hidden normal radar hip drill champion minute nurse spawn",
   },
   custom_tester_2: {
-    privateKey: "eyJuYW1lIjoic3dlZXRfd2FsbGV0IiwiYWRkcmVzcyI6InRlcnJhMTBoY3JqMHRsdW04bmtrMmFwejN2NnkwdnJkZDhrOW11cnM5dmZ5IiwiZW5jcnlwdGVkX2tleSI6ImU5N2Y4NGM0YzlmMWFjM2YxYmVhYTI4YmE5Y2QyYzNiYWE1NTQzNWM1ZjY0ZGIyNWEwMmFiMjA1N2VmMzc3YzBJVTVhRXJITm84M0FBRjlDTDR4ZFBmVXF5R0x2cG1ONGdPVk9od3dxQUxXVW4vZUZ1MSt5SVpPdW0yWXBKaW5pN2I3bkE1cDFyMUVaaU43d3JWSUZHWGNheGRYSncyOTdqL3hrUld4ZmhERT0ifQ",
+    privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
   },
 };
